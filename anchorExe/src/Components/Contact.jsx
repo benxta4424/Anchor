1045a1388d247ec83fff35a7afa3a30079926c75
benxta4424 <podean.beni@gmail.com>
2026-03-31 19:00:24 +0300

    Tying the contact section to the EmailJS tool that lets the user contact me through the website making that section useful. PS: i will be tweaking some inputs but the main important stuff for the section is done

--- anchorExe/src/Components/Contact.jsx
+++ anchorExe/src/Components/Contact.jsx
@@ -1,4 +1,35 @@
+import { useRef } from "react"
+import emailjs from '@emailjs/browser';
+import { useState } from "react";
+
 export default function Contact() {
+
+    const [isSending, setSending] = useState(false);
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+        setSending(true);
+
+        emailjs.sendForm(
+            'service_1uj8g1p', 
+            'template_ai35ivu', 
+            form.current, 
+            'kYnLJq5tgUinudjEQ')
+            
+            .then((result) => {
+                alert("Mesaj trimis cu succes! ⚡");
+                e.target.reset();
+            }
+            
+            , (error) => {
+                alert("Eroare la trimitere: " + error.text);
+            })
+            
+            .finally(() => {
+                setSending(true);
+            });
+    };
+
     return (
         <div className="contactWrapper">
             <div className="contactHeader">
@@ -6,24 +37,24 @@ export default function Contact() {
                 <p>Ai întrebări despre proiectul AnchorExe sau vrei să contribui?</p>
             </div>
 
-            <form action="" className="contactForm">
+            <form ref={form} onSubmit={sendEmail} action="" className="contactForm">
                 <div className="inputGroup">
                     <label htmlFor="name">Nume Complet</label>
-                    <input type="text" id="name" placeholder="ex: Johnson Edward" />
+                    <input type="text" id="name" name="name" placeholder="ex: Johnson Edward" required />
                 </div>
 
                 <div className="inputGroup">
                     <label htmlFor="email">Adresa Email</label>
-                    <input type="email" id="email" placeholder="ex: name@gmail.com" />
+                    <input type="email" id="email" name="user_email" placeholder="ex: name@gmail.com" required />
                 </div>
 
                 <div className="inputGroup">
                     <label htmlFor="message">Mesajul Tău</label>
-                    <textarea id="message" name="message" placeholder="Scrie aici..."></textarea>
+                    <textarea id="message" name="message" placeholder="Scrie aici..." required></textarea>
                 </div>
 
-                <button type="submit" className="submitBtn">
-                    Trimite Mesajul <span>⚡</span>
+                <button type="submit" className="submitBtn" disabled={isSending}>
+                    { isSending ? "Se trimite.. " : "Trimite Mesajul" } <span>⚡</span>
                 </button>
             </form>
         </div>
